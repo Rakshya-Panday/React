51fc2e6c030f93e319989e9792acd37d924e9d3a
Rakshya-Panday <rakshyapanday202@gmail.com>
2023-12-02 22:29:56 +0545

    complete the themeChanger project using useContext

--- contextSwitcher/src/App.jsx
+++ contextSwitcher/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ThemeButton } from './components/ThemeButton'
 import { Card } from './components/Card'
+import { ThemeProvider } from './components/Theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const[themeMode,setThememode] = useState("dark")
+ const darkTheme =()=>{
+  setThememode("dark")
+ }
+
+ const lightTheme =()=>{
+  setThememode("light")
+ }
+
+ //actually how theme change.
+
+useEffect(()=>{
+ document.querySelector('html').classList.remove("light","dark");
+ document.querySelector('html').classList.add(themeMode)
+
+
+},[themeMode])
 
   return (
-    <>
     
+    
+<ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -21,9 +39,10 @@ function App() {
                        <Card/>
                     </div>
                 </div>
-            </div>
+  </div>
+</ThemeProvider>
 
-    </>
+    
   )
 }
 
