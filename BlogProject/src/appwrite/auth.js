155ff461a7fbdadbba00260a9f333ebfeb0dba3a
Rakshya-Panday <rakshyapanday202@gmail.com>
2023-12-07 21:34:43 +0545

    Implement the header and footer components

--- BlogProject/src/appwrite/auth.js
+++ BlogProject/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from '../conf.js';
+import conf from '../conf/conf.js'
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService{
@@ -6,7 +6,7 @@ export class AuthService{
     account;
     constructor(){
         this.client
-        .setEndPoint(conf.appwriteUrl)
+        .setEndpoint(conf.appwriteUrl)
         .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
